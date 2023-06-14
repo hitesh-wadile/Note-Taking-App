edd77c08d0590266ff8c75276c57120df27fdce2
hitesh <hiteshwadile2003@gmail.com>
2023-06-14 15:33:47 +0530

    Basic Setup of the code using react

--- note-app/src/index.js
+++ note-app/src/index.js
@@ -1,63 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const App = (
-    <div>
-    <nav class="side-nav">
-        <div class="sign-up">
-            <div class="btn">
-                <button>Sign Up</button>
-            </div>
-        </div>
-        <div class="nav-elements">
-            <div class="create">
-                <div class="btn">
-                    <button>New Project</button>
-                </div>
-            </div>
-            <div class="main-items">
-                <a class=".link" href=""><i class="fas fa-magnifying-glass"></i>Search</a>
-                <a class=".link" href="">Home</a>
-                <a class=".link" href="">Notes</a>
-                <a class=".link" href="">Tasks</a>
-            </div>
-        </div>
-        <div class="features"></div>
-    </nav>
-
-    <section>
-        <div class="hero">
-            <div class="welcome-msg">
-                <h2>Welcome back, Hitesh!</h2>
-            </div>
-            <div class="project-title">
-                <p>My Projects</p>
-            </div>
-
-            <div class="my-projects">
-                <div class="single-project"></div>
-                <div class="single-project"></div>
-                <div class="single-project"></div>
-                <div class="single-project"></div>
-                <div class="single-project"></div>
-                <div class="single-project"></div>
-            </div>
-            <div class="note-title">
-                <p>My Notes</p>
-            </div>
-
-            <div class="my-note">
-                <div class="single-note"></div>
-                <div class="single-note"></div>
-                <div class="single-note"></div>
-                <div class="single-note"></div>
-                <div class="single-note"></div>
-                <div class="single-note"></div>
-            </div>
-        </div>
-    </section>
-
-    </div>
-)
-
-ReactDOM.render(<App />,document.getElementById("root"));
+// ReactDOM.render(<App />,document.getElementById("root"));
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+);
+const element = <h1>Hello, world</h1>;
+root.render(element);
